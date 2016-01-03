afe2415f47063dfa6277ead326a618f466d9e6b9
hoshinotsuyoshi <guitarpopnot330@gmail.com>
2016-01-04 08:58:54 +0900

    Rename cards -> hand

--- src/tenho/tenho_test.go
+++ src/tenho/tenho_test.go
@@ -6,15 +6,15 @@ import (
 )
 
 func ExampleHand_HaiString() {
-	cards := tenho.Hand{31, 33, 25, 11, 12, 28, 10, 24, 31, 32, 23, 21, 8, 27}
-	fmt.Println(cards.HaiString())
+	hand := tenho.Hand{31, 33, 25, 11, 12, 28, 10, 24, 31, 32, 23, 21, 8, 27}
+	fmt.Println(hand.HaiString())
 	// Output:
 	// 🀟 🀡 🀙 🀋 🀌 🀜 🀊 🀘 🀟 🀠 🀗 🀕 🀈 🀛
 }
 
 func ExampleHand_GroupSuit() {
-	cards := tenho.Hand{31, 33, 25, 11, 12, 28, 10, 24, 31, 32, 23, 21, 8, 27}
-	fmt.Println(cards.GroupSuit())
+	hand := tenho.Hand{31, 33, 25, 11, 12, 28, 10, 24, 31, 32, 23, 21, 8, 27}
+	fmt.Println(hand.GroupSuit())
 	// Output:
 	// [[] [4 5 3 1] [8 7 5] [6 8 0 3 6 7 2]]
 }
