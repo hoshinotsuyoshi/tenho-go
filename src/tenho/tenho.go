68c2d8ca3997152c26acba785c1a646971712308
hoshinotsuyoshi <guitarpopnot330@gmail.com>
2016-01-10 01:20:00 +0900

    Refactor Start()

--- src/tenho/tenho.go
+++ src/tenho/tenho.go
@@ -21,30 +21,33 @@ func Start(o OptionStruct) {
 	start := time.Now().UnixNano()
 	var i float64
 	i = 0
+	var hai string
+	var ok bool
 	for {
 		i++
-		end := time.Now().UnixNano()
-		diff := float64(end-start) / 1000000000
-		m := i / diff
-		out := 0
-		if m >= 0 {
-			out = int(m)
-		}
 		seed := time.Now().UnixNano()
-		var hai string
-		var ok bool
 		hai, ok = tryOnce(seed)
 		if int(i)%10000 == 0 {
+			end := time.Now().UnixNano()
+			diff := float64(end-start) / 1000000000
+			m := i / diff
+			out := 0
+			if m >= 0 {
+				out = int(m)
+			}
 			fmt.Printf("\r%v回試行  %v秒経過 %v回/秒 %v", i, diff, out, hai)
 		}
 		if ok {
+			end := time.Now().UnixNano()
+			diff := float64(end-start) / 1000000000
+			m := i / diff
+			out := 0
+			if m >= 0 {
+				out = int(m)
+			}
 			fmt.Printf("\r%v回試行  %v秒経過 %v回/秒 %v", i, diff, out, hai)
 			break
 		}
-		//fmt.Printf("\r%v回試行  %v秒経過 %v回/秒 %v", i, diff, out, hai)
-		//if i > 100000 {
-		//	break
-		//}
 	}
 	fmt.Printf("\n")
 }
